6abdd74349b355c20f8f48a91915027f8a305b14
sdenvs <sdenvs@gmail.com>
2021-12-03 15:09:11 +0530

    changed

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -1,1 +1,29 @@
 // Write your code here
+import './index.css'
+import {Component} from 'react'
+
+class Speedometer extends Component {
+  state = {speed: 0}
+
+  render() {
+    const {speed} = this.state
+    return (
+      <div className="bgContainer">
+        <h1 className="heading">SPEEDOMETER</h1>
+        <img
+          className="imageStyle"
+          alt="speedometer"
+          src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+        />
+        <h2 className="heading2">Speed is {speed}mph</h2>
+        <p className="paragraph">Min Limit is 0mph, Max Limit is 200mph</p>
+        <div>
+          <button className="accelButton">Accelerate</button>
+          <button className="brakeButton">Apply Brake</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Speedometer
